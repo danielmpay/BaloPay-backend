8ecb3d4073b9b666819844534b59440fc5bb0dc0
Balomock Technology <danielmpay27@gmail.com>
2026-04-30 23:24:48 +0400

    fix cors

--- server.js
+++ server.js
@@ -33,20 +33,6 @@ app.use(
 app.use(cors());
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Access, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-
-//   if (req.method === "OPTIONS") {
-//     return res.status(200).end();
-//   }
-//   next();
-// });
-
 //INIT
 const ledger = new Ledger();
 const accountRegister = new AccountRegister();
