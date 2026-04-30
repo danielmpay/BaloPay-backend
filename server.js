d7e47a7df3725a758d1ba026eb4cc55793992fba
Balomock Technology <danielmpay27@gmail.com>
2026-04-30 18:36:13 +0400

    feat: add register route with full user model

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200).end();
+    return res.status(200).end();
   }
   next();
 });
